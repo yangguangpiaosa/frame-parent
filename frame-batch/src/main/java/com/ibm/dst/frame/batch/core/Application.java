9b4821d5b0eaa5b4b53dc41cb9f01522bbcde795
yangguangpiaosa <yangguangpiaosa@163.com>
2015-06-18 16:33:42 +0800

    Using Spring Batch and Spring Boot.

---
+++ frame-batch/src/main/java/com/ibm/dst/frame/batch/core/Application.java
@@ -0,0 +1,13 @@
+package com.ibm.dst.frame.batch.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
